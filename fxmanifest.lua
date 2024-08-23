75f0d29195d679d638cdd371ca349af3cc6b1e82
Jan <Nomovilhd@gmail.com>
2024-08-23 22:15:11 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'bodacious'
 games { 'gta5' }
 
 author 'nomovil'
-description 'CopChase'
+description 'misterx'
 version '0.0.3'
 
 client_scripts {
@@ -12,21 +12,11 @@ client_scripts {
     "client_helper_fkt.lua",
     "config.lua",
     "gui_client.lua",
-    "client_itembox.lua",
-    "client_itembox_actions.lua",
     "develpoment_client.lua"
+    
 }
 
 server_scripts {
     "server_events.lua",
     "server_threads.lua"
-}
-
-ui_page 'nui/nui.html'
-
-files {
-    'nui/nui.html',
-    'nui/style.css',
-    'nui/script.js',
-    'nui/images/*'
 }
